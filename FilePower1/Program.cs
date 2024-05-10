ebdac63e8cbd67b571bba220bcd6229fdd9b3b0b
Fabio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-10 12:56:53 +0100

    Add file and cycle to write the file

---
+++ FilePower1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace FilePower1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName; 
+            string line;
+
+
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Not enough");
+                return;
+            }
+
+            fileName = args[0];
+
+            Console.WriteLine("Add text to file...");
+
+            using StreamWriter writer = new StreamWriter(fileName);
+
+
+
+                while(true)
+                {
+                    line = Console.ReadLine();
+
+                    if(line.Length <= 0)
+                        break;
+
+                    writer.WriteLine(line);
+                }
+        }
+    }
+}
