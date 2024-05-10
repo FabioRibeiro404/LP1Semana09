dd1a31968f277cff0ee78210865349d439d8cb2c
Fabio Ribeiro <fabioribeiro.info@gmail.com>
2024-05-10 11:55:57 +0100

    Add List, Stack, Queue and HashSet

---
+++ IntCollections/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int> {1, 10, -30, 10, -5};
+            Stack<int> intStack = new Stack<int> (intList);
+            Queue<int> intQueue = new Queue<int> (intList);
+            HashSet<int> intHashSet = new HashSet<int> (intList);
+
+            
+        }
+    }
+}
